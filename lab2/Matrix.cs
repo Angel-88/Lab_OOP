f4d3f8d449b477ad142afa03b97d8cd18f975442
Dasha <dasha.zagrebelna.08.01.02@gmail.com>
2020-11-17 13:52:14 +0200

    Add project files.

---
+++ lab2/Matrix.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Matrix
+    {
+        private int[,] _data;
+
+        public int Width { get; }
+
+        public int Length { get; }
+
+        public Matrix(int width, int length)
+        {
+            if (width <= 0 || length <= 0)
+            {
+                throw new ArgumentException("Input Width/Length is incorrect");
+            }
+
+            _data = new int[width, length];
+            Width = _data.GetLength(0);
+            Length = _data.GetLength(1);
+        }
+
+        public void FillMatrixByRandomValues()
+        {
+            var rand = new Random();
+            for (var i = 0; i < Width; i++)
+            {
+                for (var j = 0; j < Length; j++)
+                {
+                    _data[i, j] = rand.Next(0, 99);
+                }
+            }
+        }
+
+        public int this[int i, int j]
+        {
+            get
+            {
+                if (i < 0 || i > Width)
+                {
+                    throw new ArgumentException("Parameter i out of the bound");
+                }
+                if (j < 0 || j > Length)
+                {
+                    throw new ArgumentException("Parameter j out of the bound");
+                }
+                return _data[i, j];
+            }
+            set
+            {
+                if (i < 0 || i > Width)
+                {
+                    throw new ArgumentException("Parameter i out of the bound");
+                }
+                if (j < 0 || j > Length)
+                {
+                    throw new ArgumentException("Parameter j out of the bound");
+                }
+                _data[i, j] = value;
+            }
+        }
+
+        public int FindMaxElement()
+        {
+            var maxElement = _data[0, 0];
+            for (var i = 0; i < Width; i++)
+            {
+                for (var j = 0; j < Length; j++)
+                {
+                    if (maxElement <= _data[i, j])
+                    {
+                        maxElement = _data[i, j];
+                    }
+                }
+            }
+            return maxElement;
+        }
+
+        public int FindMixElement()
+        {
+            var mixElement = _data[0, 0];
+            for (var i = 0; i < Width; i++)
+            {
+                for (var j = 0; j < Length; j++)
+                {
+                    if (mixElement >= _data[i, j])
+                    {
+                        mixElement = _data[i, j];
+                    }
+                }
+            }
+
+            return mixElement;
+        }
+
+        public override string ToString()
+        {
+            var result = "";
+            for (var i = 0; i < Width; i++)
+            {
+                result += "[";
+                for (var j = 0; j < Length; j++)
+                {
+                    result += " ";
+                    result += _data[i, j];
+                }
+
+                result += " ]\n";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ lab2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter matrix length = ");
+            var matrixLength = int.Parse(Console.ReadLine() ?? "1");
+            Console.Write("Please enter matrix width = ");
+            var matrixWidth = int.Parse(Console.ReadLine() ?? "1");
+
+            var matrix = new Matrix(matrixWidth, matrixLength);
+            matrix.FillMatrixByRandomValues();
+
+
+            Console.WriteLine("Matrix dimensions: length = " + matrix.Length + ", width = " + matrix.Width);
+            Console.WriteLine("Matrix with random values:\n" + matrix);
+
+            // matrix[1, 1] = 50;
+            // Console.WriteLine("Matrix value at [1, 1]: " + matrix[1, 1]);
+
+            Console.WriteLine("Max matrix value = " + matrix.FindMaxElement());
+            Console.WriteLine("Min matrix value = " + matrix.FindMixElement());
+
+            Console.ReadKey();
+        }
+    }
+}
